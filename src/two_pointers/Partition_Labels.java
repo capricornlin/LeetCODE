9b59a8c9d856054f4a374ee6cc3efeec6008e1c7
Caseylin <capricornlin1989@gmail.com>
2021-03-03 11:57:20 +0800

    763. Partition Labels

---
+++ src/two_pointers/Partition_Labels.java
@@ -0,0 +1,58 @@
+package two_pointers;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Partition_Labels {
+
+        public List<Integer> partitionLabels(String S) {
+            List<Integer> res = new ArrayList<>();
+            Map<Character,Integer> hashmap = new HashMap<>();
+            int rightmost=Integer.MIN_VALUE;
+            int start_point = 0;
+            for(int i = 0; i< S.length();i++){
+                if(hashmap.containsKey(S.charAt(i))){
+                    if(hashmap.get(S.charAt(i)) < i){
+                        hashmap.put(S.charAt(i),i);
+                    }
+                }else{
+                    hashmap.put(S.charAt(i),i);
+                }
+            }
+            for(int i = 0; i < S.length();i++){
+                char ch = S.charAt(i);
+                rightmost = Math.max(rightmost,hashmap.get(ch));
+                if(i == rightmost){
+                    res.add(rightmost-start_point+1);
+                    start_point = rightmost+1;
+                }
+
+            }
+            return res;
+        }
+
+        public static void main(String[] args){
+            String s ="ababcbacadefegdehijhklij";
+            Partition_Labels p = new Partition_Labels();
+            p.partitionLabels(s);
+        }
+
+    }
+
+
+/**
+
+ S = "ababcbaca defegde hijhklij"
+ The partition is "ababcbaca", "defegde", "hijhklij".
+ 9 15
+ 1.hashmap紀錄<char,Integer> 每一個char right most的值
+ 2.time complexity: O(N)
+
+
+
+ */
+
+
+
